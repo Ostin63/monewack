3c568f2dc3b2185893d521974360487a78f0d656
Ostin63 <39737185+Ostin63@users.noreply.github.com>
2021-03-13 13:49:49 +0300

    валидация формы

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,18 @@
-const feedbackForm = modalFeedback.querySelector('.modal-feedback__form');
-const fields = feedbackForm.querySelectorAll('.feedback-item__field');
+const feedbackForm = document.querySelector('.main-feedback__form');
+const fields = feedbackForm.querySelectorAll('.feedback-field');
+const fullname = feedbackForm.querySelector('[name=fullname]');
+const phone = feedbackForm.querySelector('[name=phone]');
 
 feedbackForm.addEventListener('submit', function (evt) {
-  if (!fullname.value || !email.value) {
+  if (!fullname.value || !phone.value) {
     evt.preventDefault();
     for (let i = 0; i < fields.length; i++) {
       let field = fields[i];
       if (!field.value) {
-        field.classList.add('field-error');
-        modalFeedback.classList.remove('modal-error');
-        modalFeedback.offsetWidth = modalFeedback.offsetWidth;
-        modalFeedback.classList.add('modal-error');
+        field.classList.add('error');
       } else {
-        field.classList.remove('field-error');
+        field.classList.remove('error');
       }
     }
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem('fullname', fullname.value);
-    }
-  }
+  } 
 });
